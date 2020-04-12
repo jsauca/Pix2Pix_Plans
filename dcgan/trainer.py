d54f939a9ad5004d122ddc154eeca5dc15f063c5
Wapity / Alex <onealex.c@gmail.com>
2020-04-12 20:26:36 +0200

    Update trainer.py

--- dcgan/trainer.py
+++ dcgan/trainer.py
@@ -160,8 +160,8 @@ class Trainer:
             self._d_step(x_real.to(device))
             if np.random.uniform() < self._args.gen_prob:
                 self._g_step()
-            # if self._args.debug and batch_idx > 10:
-            #     break
+            if batch_idx > 10:
+                break
         self._lr_scheduler.step()
         self._epoch_dir = self._dir + '/epoch_{}'.format(self._epoch)
         print('--> Training epoch = {} done !'.format(self._epoch))
@@ -206,7 +206,7 @@ class Trainer:
         plt.imshow(i)
         plt.show()
         self._samples.append(i)
-        plt.clf()
+        #plt.clf()
 
     def show_animation(self):
         from IPython.display import HTML
