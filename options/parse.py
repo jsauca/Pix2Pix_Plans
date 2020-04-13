9569b0f8b6849ebc57f712ceded8bdfb3fdda6ce
Wapity / Alex <onealex.c@gmail.com>
2020-04-13 13:07:30 +0200

    Update parse.py

--- options/parse.py
+++ options/parse.py
@@ -71,11 +71,11 @@ def options_trainer(parser):
     parser.add_argument('--learning_rate_decay',
                         help='learning rate decay for optimizer',
                         type=float,
-                        default=0.999)
+                        default=0.999) #1
     parser.add_argument('--weight_decay',
                         help='weight decay for optimizer',
                         type=float,
-                        default=5e-6)
+                        default=5e-5) #0 #1e-6 #5e-6
     parser.add_argument('--num_samples',
                         help='number of samples for tester',
                         type=int,
